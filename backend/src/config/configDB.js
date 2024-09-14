9fc3965f3ec3e7cc24d7d1b4f07b3c9e9bd65b1a
HikkizZ <fmiranda.reb@gmail.com>
2024-09-13 21:46:25 -0300

    MOD configDB.js
    Se agrega configuración de la base de datos

---
+++ backend/src/config/configDB.js
@@ -0,0 +1,24 @@
+"use strict"; //! strict mode helps you to write cleaner code and avoid common mistakes.
+import { DataSource } from "typeorm"; //* It is used to configure and manage the connection to a database in projects that use TypeORM.
+import { DATABASE, BD_USERNAME, BD_PASSWORD, HOST } from "./configEnv.js";
+
+export const AppDataSource = new DataSource({
+    type: "postgres", // The type of the database you are connecting to.
+    host: `${HOST}`, // The host of the relational database
+    port: 5432, // The port of the relational database
+    username: `${BD_USERNAME}`, // The username of the relational database
+    password: `${BD_PASSWORD}`, // The password of the relational database
+    database: `${DATABASE}`, // The name of the relational database
+    synchronize: true, // Indicates if database schema should be auto created on every application launch.
+    logging: false, // Enable or disable logging of the SQL commands.
+    entities: ["src/entity/**/*.ts"], // Array of entities to load
+});
+
+export async function connectBD() {
+    try {
+        await AppDataSource.initialize(); // Initialize the connection
+        console.log("=> Database connected successfully"); // Print a message
+    } catch (error) {
+        console.log("Error connecting to the database: ", error); // Print a error message
+    }
+}
