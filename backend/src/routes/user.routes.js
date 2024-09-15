813a3a0a6057b5449b4a338ffa004b90e1b3ff4b
HikkizZ <fmiranda.reb@gmail.com>
2024-09-15 19:32:28 -0300

    ADD user.routes.js
    Rutas de usuario, contiene método post y get (Crear usuario, obtener todos los usuarios y obtener usuario por id)

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+// Importing the functions from the user controller.
+import {
+    createUser,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser
+} from '../controllers/user.controller.js';
+
+const router = Router(); //? It creates a new instance of the express router.
+
+//! http://localhost:3000/api/user
+router.post('/', createUser); //? It creates a new user. - Method: HTTP POST
+//! http://localhost:3000/api/user/all
+router.get('/all', getUsers); //? It gets all the users. - Method: HTTP GET
+//! http://localhost:3000/api/user/update/:id
+router.get('/:id', getUserById); //? It gets a user by id. - Method: HTTP GET
+
+// Exporting the routers.
+export default router;
