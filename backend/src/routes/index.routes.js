5dfdb1a3b4396432d03eb233b36a6fb875c788b7
HikkizZ <fmiranda.reb@gmail.com>
2024-09-18 12:32:03 -0300

    MOD index.routes.js
    Se añade rutas auth y se cambia ligeramente la estructura del codigo

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 // Importing routes
 import userRoutes from './user.routes.js';
+import authRoutes from './auth.routes.js';
 
 const router = Router(); //? It creates a new instance of the express router.
 
-//! http://localhost:3000/api/user
-router.use('/user', userRoutes); //? It uses the user routes.
+router
+    .use('/user', userRoutes) //! http://localhost:3000/api/user
+    .use('/auth', authRoutes); //! http://localhost:3000/api/auth
 
 // Exporting the routers
 export default router;
